1d4b06b84f61a5ede665d7307e13244e5da32d45
aidhiuwa <jaxsonroche2023@gmail.com>
2025-02-24 21:09:17 -0800

    Add files via upload

--- DBRroblox.lua
+++ DBRroblox.lua
@@ -7,9 +7,9 @@ local DBR = Instance.new("ScreenGui")
 local Background = Instance.new("Frame")
 local Holder = Instance.new("Frame")
 local UIGridLayout = Instance.new("UIGridLayout")
-local DashForwards = Instance.new("TextLabel")
 local BlindKillerButton = Instance.new("TextButton")
 local ForceWiggleButton = Instance.new("TextButton")
+local DashForwards = Instance.new("TextButton")
 
 --Properties:
 
@@ -42,21 +42,6 @@ UIGridLayout.SortOrder = Enum.SortOrder.LayoutOrder
 UIGridLayout.CellPadding = UDim2.new(0, 10, 0, 10)
 UIGridLayout.CellSize = UDim2.new(0.423000008, 0, 0.0710000023, 0)
 
-DashForwards.Name = "Dash Forwards"
-DashForwards.Parent = Holder
-DashForwards.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-DashForwards.BackgroundTransparency = 0.900
-DashForwards.BorderColor3 = Color3.fromRGB(0, 0, 0)
-DashForwards.BorderSizePixel = 0
-DashForwards.Position = UDim2.new(0.510736167, 0, 0.043524418, 0)
-DashForwards.Size = UDim2.new(0.423312873, 0, 0.0711252689, 0)
-DashForwards.Font = Enum.Font.Roboto
-DashForwards.Text = "Dash Forwards (E)"
-DashForwards.TextColor3 = Color3.fromRGB(1, 46, 85)
-DashForwards.TextScaled = true
-DashForwards.TextSize = 14.000
-DashForwards.TextWrapped = true
-
 BlindKillerButton.Name = "Blind Killer Button"
 BlindKillerButton.Parent = Holder
 BlindKillerButton.BackgroundColor3 = Color3.fromRGB(131, 133, 136)
@@ -83,9 +68,22 @@ ForceWiggleButton.TextScaled = true
 ForceWiggleButton.TextSize = 14.000
 ForceWiggleButton.TextWrapped = true
 
+DashForwards.Name = "Dash Forwards"
+DashForwards.Parent = Holder
+DashForwards.BackgroundColor3 = Color3.fromRGB(131, 133, 136)
+DashForwards.BorderColor3 = Color3.fromRGB(0, 0, 0)
+DashForwards.BorderSizePixel = 0
+DashForwards.Size = UDim2.new(0, 200, 0, 50)
+DashForwards.Font = Enum.Font.SourceSans
+DashForwards.Text = "Dash forwards"
+DashForwards.TextColor3 = Color3.fromRGB(1, 46, 85)
+DashForwards.TextScaled = true
+DashForwards.TextSize = 14.000
+DashForwards.TextWrapped = true
+
 -- Scripts:
 
-local function YDPWQRL_fake_script() -- Holder.Script 
+local function DTEC_fake_script() -- Holder.Script 
 	local script = Instance.new('Script', Holder)
 
 	local UIS = game:GetService("UserInputService")
@@ -122,26 +120,8 @@ local function YDPWQRL_fake_script() -- Holder.Script
 		end
 	end)
 end
-coroutine.wrap(YDPWQRL_fake_script)()
-local function FUGJ_fake_script() -- DashForwards.Script 
-	local script = Instance.new('Script', DashForwards)
-
-	local UIS = game:GetService("UserInputService")
-	local Players = game:GetService("Players")
-	
-	local Plr = Players.LocalPlayer
-	
-	
-	UIS.InputBegan:Connect(function(key, e)
-		if key.KeyCode == Enum.KeyCode.E and e then
-			local Character = Plr.Character
-			
-			Character:PivotTo(Character:GetPivot() * CFrame.new(0, 0, -5))
-		end
-	end)
-end
-coroutine.wrap(FUGJ_fake_script)()
-local function WWVSXGN_fake_script() -- BlindKillerButton.Script 
+coroutine.wrap(DTEC_fake_script)()
+local function XNVNUTA_fake_script() -- BlindKillerButton.Script 
 	local script = Instance.new('Script', BlindKillerButton)
 
 	local Players = game:GetService("Players")
@@ -179,8 +159,8 @@ local function WWVSXGN_fake_script() -- BlindKillerButton.Script
 	
 	end)
 end
-coroutine.wrap(WWVSXGN_fake_script)()
-local function VJMOBEU_fake_script() -- ForceWiggleButton.Script 
+coroutine.wrap(XNVNUTA_fake_script)()
+local function AEJH_fake_script() -- ForceWiggleButton.Script 
 	local script = Instance.new('Script', ForceWiggleButton)
 
 	local Players = game:GetService("Players")
@@ -221,8 +201,21 @@ local function VJMOBEU_fake_script() -- ForceWiggleButton.Script
 		until count >= 200
 	end)
 end
-coroutine.wrap(VJMOBEU_fake_script)()
-local function XKDESYS_fake_script() -- DBR.Script 
+coroutine.wrap(AEJH_fake_script)()
+local function JDNTERV_fake_script() -- DashForwards.Script 
+	local script = Instance.new('Script', DashForwards)
+
+	local Players = game:GetService("Players")
+	
+	local Plr = Players.LocalPlayer
+	local Character = Plr.Character
+	
+	script.Parent.MouseButton1Click:Connect(function()
+		Character:PivotTo(Character:GetPivot() * CFrame.new(0, 0, -8))
+	end)
+end
+coroutine.wrap(JDNTERV_fake_script)()
+local function GNGQEPF_fake_script() -- DBR.Script 
 	local script = Instance.new('Script', DBR)
 
 	local UIS = game:GetService("UserInputService")
@@ -233,4 +226,4 @@ local function XKDESYS_fake_script() -- DBR.Script
 		end
 	end)
 end
-coroutine.wrap(XKDESYS_fake_script)()
+coroutine.wrap(GNGQEPF_fake_script)()
