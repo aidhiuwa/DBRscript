1e2e0f14cff088d717c536269bc34f17c7eceb2d
aidhiuwa <jaxsonroche2023@gmail.com>
2025-02-24 21:45:04 -0800

    Add files via upload

--- DBRroblox.lua
+++ DBRroblox.lua
@@ -9,7 +9,7 @@ local Holder = Instance.new("Frame")
 local UIGridLayout = Instance.new("UIGridLayout")
 local BlindKillerButton = Instance.new("TextButton")
 local ForceWiggleButton = Instance.new("TextButton")
-local DashForwards = Instance.new("TextButton")
+local UnhookAll = Instance.new("TextButton")
 
 --Properties:
 
@@ -68,22 +68,22 @@ ForceWiggleButton.TextScaled = true
 ForceWiggleButton.TextSize = 14.000
 ForceWiggleButton.TextWrapped = true
 
-DashForwards.Name = "Dash Forwards"
-DashForwards.Parent = Holder
-DashForwards.BackgroundColor3 = Color3.fromRGB(131, 133, 136)
-DashForwards.BorderColor3 = Color3.fromRGB(0, 0, 0)
-DashForwards.BorderSizePixel = 0
-DashForwards.Size = UDim2.new(0, 200, 0, 50)
-DashForwards.Font = Enum.Font.SourceSans
-DashForwards.Text = "Dash forwards"
-DashForwards.TextColor3 = Color3.fromRGB(1, 46, 85)
-DashForwards.TextScaled = true
-DashForwards.TextSize = 14.000
-DashForwards.TextWrapped = true
+UnhookAll.Name = "Unhook All"
+UnhookAll.Parent = Holder
+UnhookAll.BackgroundColor3 = Color3.fromRGB(131, 133, 136)
+UnhookAll.BorderColor3 = Color3.fromRGB(0, 0, 0)
+UnhookAll.BorderSizePixel = 0
+UnhookAll.Size = UDim2.new(0, 200, 0, 50)
+UnhookAll.Font = Enum.Font.SourceSans
+UnhookAll.Text = "Unhook All Hooked Players"
+UnhookAll.TextColor3 = Color3.fromRGB(1, 46, 85)
+UnhookAll.TextScaled = true
+UnhookAll.TextSize = 14.000
+UnhookAll.TextWrapped = true
 
 -- Scripts:
 
-local function KBDTEOL_fake_script() -- Holder.Script 
+local function FMZU_fake_script() -- Holder.Script 
 	local script = Instance.new('Script', Holder)
 
 	local UIS = game:GetService("UserInputService")
@@ -120,8 +120,8 @@ local function KBDTEOL_fake_script() -- Holder.Script
 		end
 	end)
 end
-coroutine.wrap(KBDTEOL_fake_script)()
-local function TEVZ_fake_script() -- BlindKillerButton.Script 
+coroutine.wrap(FMZU_fake_script)()
+local function NDPI_fake_script() -- BlindKillerButton.Script 
 	local script = Instance.new('Script', BlindKillerButton)
 
 	local Players = game:GetService("Players")
@@ -162,8 +162,8 @@ local function TEVZ_fake_script() -- BlindKillerButton.Script
 	
 	end)
 end
-coroutine.wrap(TEVZ_fake_script)()
-local function NQGQS_fake_script() -- ForceWiggleButton.Script 
+coroutine.wrap(NDPI_fake_script)()
+local function BEMXI_fake_script() -- ForceWiggleButton.Script 
 	local script = Instance.new('Script', ForceWiggleButton)
 
 	local Players = game:GetService("Players")
@@ -204,21 +204,33 @@ local function NQGQS_fake_script() -- ForceWiggleButton.Script
 		until count >= 200
 	end)
 end
-coroutine.wrap(NQGQS_fake_script)()
-local function KFWIP_fake_script() -- DashForwards.Script 
-	local script = Instance.new('Script', DashForwards)
+coroutine.wrap(BEMXI_fake_script)()
+local function EXDGDTM_fake_script() -- UnhookAll.Script 
+	local script = Instance.new('Script', UnhookAll)
 
-	local Players = game:GetService("Players")
+	local function UnhookEvent(Plr, Hook)
+		local args = {
+			[1] = "Hook",
+			[2] = "Unhook",
+			[3] = Plr.Character, -- plr character
+			[4] = Hook
+		}
 	
-	local Plr = Players.LocalPlayer
-	local Character = Plr.Character
+		game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("Server_Event"):FireServer(unpack(args))
+	end
 	
 	script.Parent.MouseButton1Click:Connect(function()
-		Character:PivotTo(Character:GetPivot() * CFrame.new(0, 0, -8))
+		for i, hook in pairs(game.Workspace:GetChildren()) do
+			if string.find(hook.Name, "Hook") then
+				if game.Players:FindFirstChild(hook.Panel.Player.Value) then
+					UnhookEvent(game.Players:FindFirstChild(hook.Panel.Player.Value), hook)
+				end
+			end
+		end
 	end)
 end
-coroutine.wrap(KFWIP_fake_script)()
-local function INSCZZG_fake_script() -- DBR.Script 
+coroutine.wrap(EXDGDTM_fake_script)()
+local function PMIFJA_fake_script() -- DBR.Script 
 	local script = Instance.new('Script', DBR)
 
 	local UIS = game:GetService("UserInputService")
@@ -229,4 +241,4 @@ local function INSCZZG_fake_script() -- DBR.Script
 		end
 	end)
 end
-coroutine.wrap(INSCZZG_fake_script)()
+coroutine.wrap(PMIFJA_fake_script)()
